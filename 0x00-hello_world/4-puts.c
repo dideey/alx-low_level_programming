e9a90c8b79699e94c2fa0438282873153f6469a1
dideey <bradleyjd32@gmail.com>
2023-02-16 11:47:32 -0800

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[8] = "Programming is like building a multilingul puzzel";
+puts(str);
+return (0);
+}
