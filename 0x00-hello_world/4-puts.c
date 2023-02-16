466c25f36b28c9b3e2a9f840acbebe5b9d9192f3
dideey <bradleyjd32@gmail.com>
2023-02-16 12:01:58 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  *
  * Return: Always 0 (success)
  */
+# include <stdio.h>
 int main(void)
 {
-char str[8] = "Programming is like building a multilingul puzzel";
+char str[] = "Programming is like building a multilingul puzzel";
 puts(str);
 return (0);
 }
