a1e718eaa643ee6255f19427db2566161569b4e6
dideey <bradleyjd32@gmail.com>
2023-02-23 06:33:31 -0800

    update

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - outputs a square
+ * Return: void
+ * @size: the box size
+ */
+
+void print_square(int size)
+{
+if (size > 0)
+{
+_putchar('#');
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+return;
+}
