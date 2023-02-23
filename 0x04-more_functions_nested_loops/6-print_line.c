6fbc18e93c4f98462975b6665300f35e0f26833d
dideey <bradleyjd32@gmail.com>
2023-02-23 05:53:30 -0800

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - outputs a lin in the terminal
+ * Return: void
+ * @n: is the line lenght
+ */
+
+void print_line(int n)
+{
+if (n > 0)
+{
+_putchar('-');
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+return;
+}
