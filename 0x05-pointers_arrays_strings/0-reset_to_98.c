718286224c0c0849ca6b83c8ec2e2b23ee6817d1
dideey <bradleyjd32@gmail.com>
2023-02-27 04:26:20 -0800

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it takes a pointer and updates it to 98 .
+ * return: null
+ * @n: a parameter
+ *
+ */
+
+void reset_to_98(int *n)
+{
+if (n)
+
+*n = 98;
+
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+fun with pointers and arrays
