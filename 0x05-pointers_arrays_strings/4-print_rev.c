c04a63f004df44ae35c4f7811be1cc91628b1a57
dideey <bradleyjd32@gmail.com>
2023-02-27 08:29:47 -0800

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - it outputs a string backwards
+ * @s: the string being passed.
+ * Return: null
+ */
+
+void print_rev(char *s)
+{
+
+for (; *s == '\0'; s--)
+{
+_putchar(*s);
+}
+_putchar('\n');
+return;
+
+}
