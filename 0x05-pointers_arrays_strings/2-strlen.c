8ea7f8bc50f9595af1490bbcea3906218217ea68
dideey <bradleyjd32@gmail.com>
2023-02-27 05:49:05 -0800

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - it tskes a string as an input and states its lenght .
+ * @s: is the input string.
+ * Return: the length of the string .
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0;  *s != '\0'; i++)
+{
+_putchar(i);
+}
+return(i); 
+}
