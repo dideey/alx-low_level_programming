2562d1568232953d7ab63f41d433699d07831cfb
dideey <bradleyjd32@gmail.com>
2023-03-02 14:10:29 -0800

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,14 @@ int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 
-for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
 
-if (s1[i] == s2[i])
-{
-_putchar(0);
-}
-else if (s1[i] < s2[i])
-{
-_putchar(-15);
-}
-else
-{
-_putchar(15);
 }
 return (0);
 }
