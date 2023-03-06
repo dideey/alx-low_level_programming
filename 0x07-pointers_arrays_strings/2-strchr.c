59c0e4b3e761ae402b1289dda58538b365486dc1
dideey <bradleyjd32@gmail.com>
2023-03-06 07:03:35 -0800

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strch - searches for a string
+ * @s:string being analysed
+ * @c: the character being searched
+ * Return:c if the character is found else NULL
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int i;
+char m = c;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == m)
+{
+return (s);
+break;
+}
+
+}
+return (NULL);
+}
+
+
+
