deac6630208ece575e6097370b56c9e6bcf3087b
dideey <bradleyjd32@gmail.com>
2023-03-08 05:16:26 -0800

    update

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - reverses a string
+ * @s: the string input
+ * Return: null
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s - 1);
+_putchar(*s);
+}
+return;
+
+}
+
