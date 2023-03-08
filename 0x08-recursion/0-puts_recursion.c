ede1180192a3fa668efb2ad62e77b6f7a0ab76ac
dideey <bradleyjd32@gmail.com>
2023-03-08 03:15:06 -0800

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - outputs a string
+ * @s: input string
+ * Return: nill
+ *
+ */
+void _puts_recursion(char *s)
+{
+int i = 0;
+if (s[i] != '\0')
+{
+_putchar(s[i]);
+}
+i++;
+return;
+}
