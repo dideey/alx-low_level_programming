3782e392570e37df85863348e5f7087ffd095465
dideey <bradleyjd32@gmail.com>
2023-03-09 01:25:57 -0800

    update

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_palindrome - checks for empty string
+ * @s:input string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+
+if (*s == '\0')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
