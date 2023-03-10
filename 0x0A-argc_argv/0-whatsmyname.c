d3d1464c7427b472505259b993c33ae25a6e4c44
dideey <bradleyjd32@gmail.com>
2023-03-10 08:05:10 -0800

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mynameis - prints function name
+ * @argc:count
+ * @argv:string
+ * Return:0 success
+ */
+
+int mynameis(int __attribute__ ((unused)) argc, char *argv[])
+{
+_putchar(*argv[0]);
+return (0);
+}
