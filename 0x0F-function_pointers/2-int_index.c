2c23fbb637a7d10d5635fea91bae944d0ed75c86
dideey <bradleyjd32@gmail.com>
2023-03-22 08:34:53 -0700

    update

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for a digit
+ * @array:input
+ * @size:array size
+ * @cmp:comperison function
+ * Return:null 0r -1
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+	return (-1);
+
+for (i = 0; i < size; i++)
+	cmp(array[i]);
+return (array[i]);
+
+if (cmp == 0)
+	return (-1);
+
+
+}
