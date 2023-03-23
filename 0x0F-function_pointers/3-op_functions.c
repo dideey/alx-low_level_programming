67a54254c867092ecffea35bbc079c0479713546
dideey <bradleyjd32@gmail.com>
2023-03-23 01:40:03 -0700

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,25 +1,50 @@
 #include "3-calc.h"
-
+/**
+ * op_add - addition
+ * Return:ans
+ * @a:int
+ * @b:int2
+ */
 int op_add(int a, int b)
 {
 return (a + b);
 }
-
+/**
+ * op_sub - subtract
+ * Return:ans
+ * @a:int
+ * @b:int2
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
-
+/**
+ * op_mul - multiplication
+ * Return:ans
+ * @a:int
+ * @b:int2
+ */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
-
+/**
+ * op_div - division
+ * Return:ans
+ * @a:int
+ * @b:int2
+ */
 int op_div(int a, int b)
 {
 return (a / b);
 }
-
+/**
+ * op_mod - modulus
+ * Return:ans
+ * @a:int
+ * @b:int2
+ */
 int op_mod(int a, int b)
 {
 return (a % b);
