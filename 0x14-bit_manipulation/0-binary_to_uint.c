4871814b65c34faedef529a3b99ca36e1c4d715d
dideey <bradleyjd32@gmail.com>
2023-04-06 11:10:25 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int len, powa, sum;
-len = strlen(b) - 1;
+int len, powa, sum, i;
+len = strlen(b);
 powa = 1;
 sum = 0;
 
-if (b[len] != '1' || b[len] != '0')
-{
-	return (0);
-}
 if (b == NULL)
 {
-	return (0);
+        return (0);
+}
+{
+if (b[len] != '1' || b[len] != '0')
+{
+        return (0);
 }
 
-for (; len >= 0; len--)
+for (i = (len - 1); i  >= 0; len--)
+{
+
 if (b[len] == '1')
 {
 	sum += powa;
 	powa *= 2;
 }
-
-
+}
 return (sum);
+}
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints binary form of input
+ * @n:input
+ * Retutn:void
+ */
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+{
+print_binary(n >> 1);
+}
+
+printf("%ld", n & 1);
+
+return;
+}
