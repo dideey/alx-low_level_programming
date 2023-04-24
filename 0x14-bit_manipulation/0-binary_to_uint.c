eb6ed28827a1c0f29af9904f5455a1507f29c9da
dideey <bradleyjd32@gmail.com>
2023-04-24 03:06:30 -0700

    reviving the incomplete functions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,28 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int len, powa, sum, i;
-len = strlen(b);
+unsigned int powa, sum;
+int len;
 powa = 1;
 sum = 0;
 
 if (b == NULL)
 {
-        return (0);
+	return (0);
 }
 
+for (len = 0; b[len];)
+{
+	len++;
+}
 
-for (i = (len - 1); i >= 0; i--)
+for (len -= 1; len >= 0; len--)
 {
-if (b[i] == 1)
-{
-	sum += powa;
+	if (b[len] != '0' && b[len] != '1')
+	return (0);
+	sum += (b[len] - '0') * powa;
 	powa *= 2;
-}
+
 }
 return (sum);
 }
