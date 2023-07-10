a3b80f4a5be29182f9e623783de693888c944a9a
dideey <bradleyjd32@gmail.com>
2023-07-10 05:48:30 -0700

    file i\o

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+char *buffer_size(char *file);
+void close_file(int fd);
+
+
+
+
+
+#endif
